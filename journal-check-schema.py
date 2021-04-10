20f54bc893540d5eeaeb0b7d4ae7e2ba362df871
Athanasius <github@miggy.org>
2021-04-10 12:30:26 +0100

    Initial journal-check-schema.py
    This is heavily based on find-new-events.py.  Doesn't yet have any
    actual schema validation added.

---
+++ journal-check-schema.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+"""Check a Journal file for schema compliance."""
+import argparse
+import json
+import logging
+import os
+import pathlib
+import re
+import sys
+import yaml
+from enum import Enum
+
+APPNAME = 'journal-check-schema'
+# CONFIG_FILE = 'find-new-events.yml'
+
+
+class ErrorCodes(Enum):
+    OK = 0
+    BAD_LOGLEVEL = 1
+    NOT_DIR_OR_FILE = 2
+    BAD_CONFIG_FILE = 3
+    NO_FILES = 4
+    CONFIG_FILE_NOT_FOUND = 5
+
+class JournalSchemaCheck:
+    """Scan journals."""
+
+    config = None
+    logger = None
+    _RE_ED_JOURNAL = re.compile(r'^Journal(Alpha|Beta)?\.[0-9]{12}\.[0-9]{2}\.log$')
+    unknown_events = {}
+
+    def __init__(self):
+        """Perform initial setup."""
+        self.parser = argparse.ArgumentParser(
+            prog=APPNAME,
+            description='Scan directories and files for Elite Dangerous Journal files '
+                        'and report on any schema violations.'
+        )
+
+        self.parser.add_argument(
+            '--loglevel',
+            help='Set the log level to one of: '
+                 'CRITICAL, ERROR, WARNING, INFO, DEBUG'
+        )
+
+        self.parser.add_argument(
+            '--config',
+            help='Specify an alternative config file'
+        )
+
+        self.parser.add_argument(
+            '--errorcodes',
+            action='store_true',
+            help='Print a list of the exit error codes'
+        )
+
+        self.parser.add_argument(
+            'files',
+            help='Directories and files to be scanned',
+            nargs='*'
+        )
+
+        self.args = self.parser.parse_args()
+
+        if self.args.errorcodes:
+            for e in ErrorCodes.__members__.values():
+                print(f'{e.name:30} {e.value}')
+
+            exit(ErrorCodes.OK.value)
+
+        self.logger = logging.getLogger(APPNAME)
+        self.logger_ch = logging.StreamHandler()
+        self.logger_formatter = logging.Formatter('%(asctime)s - %(levelname)8s - %(module)s:%(lineno)d: %(message)s')
+        self.logger_formatter.default_time_format = '%Y-%m-%d %H:%M:%S';
+        self.logger_formatter.default_msec_format = '%s.%03d'
+        self.logger_ch.setFormatter(self.logger_formatter)
+        self.logger.addHandler(self.logger_ch)
+        if self.args.loglevel:
+            try:
+                self.logger.setLevel(self.args.loglevel)
+
+            except ValueError:
+                print(f'Unknown loglevel: {self.args.loglevel}')
+                self.parser.print_help()
+                exit(ErrorCodes.BAD_LOGLEVEL.value)
+
+        else:
+            self.logger.setLevel(logging.INFO)
+
+        if self.args.config:
+            config_file = pathlib.Path(self.args.config).expanduser()
+        else:
+            config_file = (pathlib.Path(sys.argv[0]).parent / CONFIG_FILE).expanduser()
+
+        try:
+            with config_file.open('r') as cf:
+                self.config = yaml.safe_load(cf)
+
+        except FileNotFoundError as e:
+            print(f'Bad config file "{config_file}": {e!r}')
+            exit(ErrorCodes.CONFIG_FILE_NOT_FOUND.value)
+
+        if len(self.args.files) == 0:
+            self.logger.error('You must specify at least one file or directory')
+            self.parser.print_help()
+            exit(ErrorCodes.NO_FILES.value)
+
+    def scan_files(self):
+        """Perform scan of all specified files."""
+        for f in self.args.files:
+            file = pathlib.Path(f)
+            file = file.expanduser()
+            self.process_file(file)
+
+    def process_file(self, file):
+        """Process a directory or single file."""
+        if file.is_dir():
+            for e in file.iterdir():
+                self.process_file(e)
+
+            return
+
+        if file.is_file():
+            if self._RE_ED_JOURNAL.search(file.name):
+                self.scan_file(file)
+
+            else:
+                self.logger.info(f'Skipping non-Journal file: "{file}"')
+
+            return
+
+        self.logger.error(f'Not a directory or plain file: "{file}"')
+        return
+
+
+    def scan_file(self, file):
+        """Check a file against schema."""
+        self.logger.debug(f'Processing "{file}"')
+        with file.open('r', encoding='utf-8') as f:
+            lineno = 0
+            for l in f:
+                lineno += 1
+                try:
+                    entry = json.loads(l)
+
+                except json.decoder.JSONDecodeError as e:
+                    self.logger.exception(f'Line:\n{l}')
+                    next
+
+                self.logger.debug(f'entry:\n{entry}')
+                # Validate
+
+
+if __name__ == '__main__':
+    scanner = JournalSchemaCheck()
+    scanner.scan_files()
+
+    exit(ErrorCodes.OK.value)
